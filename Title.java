f70b1d033a951250250bc0e5c14ad4d8430e7e31
Nozomu-Yokota <349888354@gapps.yrdsb.ca>
2022-12-13 16:31:59 -0500

    add how to move Elephant on Title screen

--- Title.java
+++ Title.java
@@ -18,7 +18,7 @@ public class Title extends World
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
 
-        addObject(titleLabel, getWidth()/2, getHeight()/2);
+        addObject(titleLabel, 260, 90);
         prepare();
     }
 
@@ -30,13 +30,32 @@ public class Title extends World
             Greenfoot.setWorld(gameWorld);
         }
     }
-    
+
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+
     /**
      * Prepare the world for the start of the program.
      * That is: create the initial objects and add them to the world.
      */
     private void prepare()
     {
-        
+        Elephant elephant = new Elephant();
+        addObject(elephant,540,45);
+        elephant.setLocation(556,85);
+        Label label = new Label("Press <Space> to Start", 40);
+        addObject(label,252,292);
+        label.setLocation(339,297);
+        label.setLocation(331,332);
+        label.setLocation(331,332);
+        Label label2 = new Label("Use <A> to move left <D> to move right", 40);
+        addObject(label2,292,186);
+        label2.setLocation(298,180);
+        Label label3 = new Label("<S> to move down <W> to move up", 40);
+        addObject(label3,305,246);
+        label3.setLocation(287,237);
+        label.setLocation(306,324);
     }
 }
